8fd37f31fa795eafe423ce8ff03633a934da2ffc
kumu <kumu@2dfire.com>
2017-12-20 15:34:30 +0800

    [add] first commit

---
+++ ImproveCodeQuality151/src/com/chiclaim/quality151/Case28.java
@@ -0,0 +1,12 @@
+package com.chiclaim.quality151;
+
+/**
+ * 优先使用整型池
+ * Created by Chiclaim on 2017/12/20.
+ */
+public class Case28 {
+
+    public static void main(String[] args) {
+        System.out.print("....");
+    }
+}
