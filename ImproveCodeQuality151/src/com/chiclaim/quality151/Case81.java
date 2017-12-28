ce3b7738f606e4a570a772d265ef580575373b61
kumu <kumu@2dfire.com>
2017-12-28 15:37:30 +0800

    [update Case81

--- ImproveCodeQuality151/src/com/chiclaim/quality151/Case81.java
+++ ImproveCodeQuality151/src/com/chiclaim/quality151/Case81.java
@@ -4,6 +4,7 @@ import java.util.SortedSet;
 import java.util.TreeSet;
 
 /**
+ * 非稳定排序推荐使用List
  * Created by Chiclaim on 2017/12/28.
  */
 public class Case81 {
@@ -16,8 +17,13 @@ public class Case81 {
         set.add(new Person(175));
 
         //修改身高
-        set.first().setHeight(185);//我测试运行是生效的，书上上说不生效，可能JDK版本不一致的原因
+        set.first().setHeight(185);//书上运行的结果是[175,、180]，我运行的结果是[185、180]，不管怎么样都没从新排序
 
+        //1，set重排序
+        //set=new TreeSet<>(new ArrayList<Person>(set));
+
+        //2，或者List的compare排序
+        //ignore code
 
         for (Person p : set) {
             System.out.println("身高：" + p.getHeight());
