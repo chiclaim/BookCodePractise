aef6d56a6c7b60d9ea130aec03a872a3dded61d1
kumu <kumu@2dfire.com>
2018-03-19 14:47:24 +0800

    [update] update desc

--- DataStructure/src/com/chiclaim/data/structure/tree/LoopTree.java
+++ DataStructure/src/com/chiclaim/data/structure/tree/LoopTree.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 二叉树的遍历
+ * 二叉树的遍历（深度优先、广度优先）
  * Created by Chiclaim on 2018/3/19.
  */
 public class LoopTree {
@@ -86,7 +86,7 @@ public class LoopTree {
     }
 
     /**
-     * 广度优先遍历又称为按层遍历，先遍历第一层（根节点），然后第二层....一次类推
+     * 广度优先遍历又称为按层遍历，先遍历第一层（根节点），然后第二层....以次类推
      * <p>
      * 可以借助FIFO队列来实现
      *
