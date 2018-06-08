bc9ee2befab91627d22a3c798d523ef2a3174315
kumu <kumu@2dfire.com>
2018-06-08 11:06:13 +0800

    [add] Set的不同实现 性能对比分析 Signed-off-by: kumu <kumu@2dfire.com>

---
+++ DataStructure/src/com/chiclaim/data/structure/set/Main.java
@@ -0,0 +1,47 @@
+package com.chiclaim.data.structure.set;
+
+import java.util.ArrayList;
+
+/**
+ * 二分搜索树实现的Set和链表实现的Set性能对比
+ * <p>
+ * Created by Chiclaim on 2018/6/8.
+ */
+public class Main {
+
+
+    private static double test(Set<String> set, String filename) {
+        long start = System.nanoTime();
+        System.out.println(filename);
+        ArrayList<String> words1 = new ArrayList<>();
+        if (FileOperation.readFile(filename, words1)) {
+            System.out.println("\tTotal words: " + words1.size());
+
+            for (String word : words1)
+                set.add(word);
+            System.out.println("\tTotal different words: " + set.size());
+        }
+
+        long end = System.nanoTime();
+
+        return (end - start) / 1000000000.0;
+    }
+
+
+    public static void main(String[] args) {
+        String filename = "pride-and-prejudice.txt";
+        double bstTime = test(new BSTSet<>(), filename);
+
+        System.out.println();
+
+        double linkedListTime = test(new LinkedListSet<>(), filename);
+
+        System.out.println();
+
+        System.out.println("BSTSet        Time: " + bstTime);
+        System.out.println("LinkedListSet Time: " + linkedListTime);
+
+    }
+
+
+}
