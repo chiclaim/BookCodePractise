58fe760ee27a8fd3a2d577253b27c2fa1782ddc2
yuzhiqiang <yuzhiqiang400@163.com>
2018-06-16 10:07:51 +0800

    [test] test trie set Signed-off-by: yuzhiqiang <yuzhiqiang400@163.com>

--- DataStructure/src/com/chiclaim/data/structure/set/Main.java
+++ DataStructure/src/com/chiclaim/data/structure/set/Main.java
@@ -11,45 +11,48 @@ public class Main {
 
 
     private static double countTime(Set<String> set, String filename) {
-        long start = System.nanoTime();
         System.out.println(filename);
         ArrayList<String> words1 = new ArrayList<>();
         if (FileOperation.readFile(filename, words1)) {
             System.out.println("\tTotal words: " + words1.size());
-
+            long start = System.nanoTime();
             for (String word : words1) {
                 set.add(word);
             }
+            System.out.println("\tTotal different words: " + set.size());
+
+            //long start = System.nanoTime();
             for (String word : words1) {
                 set.contains(word);
             }
-            System.out.println("\tTotal different words: " + set.size());
+            long end = System.nanoTime();
+            return (end - start) / 1000000000.0;
         }
+        throw new IllegalStateException();
 
-        long end = System.nanoTime();
 
-        return (end - start) / 1000000000.0;
     }
 
 
     public static void main(String[] args) {
-        //String filename = "both-of-all.txt";
-        //String filename = "pride-and-prejudice.txt";
-        String filename = "a-tale-of-two-cities.txt";
+//        String filename = "both-of-all.txt";
+        String filename = "war-and-peace.txt";
+//        String filename = "pride-and-prejudice.txt";
+//        String filename = "a-tale-of-two-cities.txt";
         double bstTime = countTime(new BSTSet<>(), filename);
         System.out.println();
 
         double trieTime = countTime(new TrieSet(), filename);
         System.out.println();
 
-        double linkedListTime = countTime(new LinkedListSet<>(), filename);
+        //double linkedListTime = countTime(new LinkedListSet<>(), filename);
 
 
         System.out.println();
 
         System.out.println("TrieSet       Time: " + trieTime);
         System.out.println("BSTSet        Time: " + bstTime);
-        System.out.println("LinkedListSet Time: " + linkedListTime);
+        //System.out.println("LinkedListSet Time: " + linkedListTime);
 
     }
 
