392e13895ffb4d863e923f1c046549634cfc587c
yuzhiqiang <yuzhiqiang400@163.com>
2018-06-17 16:06:33 +0800

    [add] 并查集 Signed-off-by: yuzhiqiang <yuzhiqiang400@163.com>

---
+++ DataStructure/src/com/chiclaim/data/structure/tree/uf/UnionFind1.java
@@ -0,0 +1,49 @@
+package com.chiclaim.data.structure.tree.uf;
+
+/**
+ * 并查集
+ * <p>
+ * 使用数组来实现，find操作为 O(1)，union操作为 O(n)
+ * <p>
+ * quick find
+ */
+public class UnionFind1 implements UF {
+
+    private int[] array;
+
+    public UnionFind1(int size) {
+        array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+    }
+
+
+    @Override
+    public int size() {
+        return array.length;
+    }
+
+    private int find(int p) {
+        return array[p];
+    }
+
+    @Override
+    public boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    @Override
+    public void unionElements(int p, int q) {
+        int pID = find(p);
+        int qID = find(q);
+        if (qID == pID) {//如果本身就是相连的
+            return;
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == pID) {
+                array[i] = qID;
+            }
+        }
+    }
+}
